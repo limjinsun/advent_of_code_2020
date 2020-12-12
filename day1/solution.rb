40456a401d8f0b6c036da8267a962de6b24c7c99
Jin Lim <loveandpeace.jin@gmail.com>
2020-12-12 22:41:35 +0000

    day-01

--- day1/solution.rb
+++ day1/solution.rb
@@ -1,4 +1,22 @@
+# Find the two entries that sum to 2020; what do you get if you multiply them together?
+
 file = File.open("puzzle_input.txt")
 file_data = file.readlines.map(&:chomp)
+data = file_data.map{ |e| e.to_i }.sort!
 
-puts file_data.inspect
+head = data.first
+tail = data.last
+
+result = head + tail
+
+until result == 2020
+  if result < 2020
+    head = data[data.find_index(head) + 1]
+  else
+    tail = data[data.find_index(tail) - 1]
+  end
+  result = head + tail
+end
+
+puts head * tail
+
